91530e6cc78df559fbb8d19110ef9512f49561e3
Pavel Rosputko <pavel.rosputko@gmail.com>
2013-03-06 19:38:25 +0400

    Resolves #13: Add server_ec2

---
+++ lib/mana/server_ec2.rb
@@ -0,0 +1,11 @@
+require 'right_aws'
+
+module Capistrano
+  class Configuration
+    def server_ec2 instance_id, *roles
+      ec2 = RightAws::Ec2.new aws[:access_key_id], aws[:secret_access_key]
+      addr = ec2.describe_instances(instance_id).first[:ip_address] 
+      server addr, *roles
+    end
+  end
+end
